0b8cadbdd661708ed864f19c15cabcc6bf293598
Sanya124 <tolstihin1996@mail.ru>
2023-11-10 00:26:22 +0700

    [IL-5] JWT token should be deactivated after logout
    - A test for logout has been developed;
    - Describe endpoint for logout.

--- framework/endpoints/authenticate_api.py
+++ framework/endpoints/authenticate_api.py
@@ -1,5 +1,4 @@
 import json
-from typing import Optional
 
 import requests
 from requests import Response
@@ -12,6 +11,35 @@ class AuthenticateAPI:
         """Initializing parameters for request"""
         self.url = HOST + "/api/v1/auth"
         self.headers = {"Content-Type": "application/json"}
+
+    def authentication(self, email: str, password: str) -> Response:
+        """Endpoint for authentication of user
+
+        Args:
+            email:    username
+            password: password for username
+        """
+        data = {
+            "email": email,
+            "password": password,
+        }
+        path = self.url + "/authenticate"
+        response = requests.post(url=path, data=json.dumps(data), headers=self.headers)
+
+        return response
+
+    def logout(self, token: str) -> Response:
+        """User logout
+
+        Args:
+            token: JWT token for authorization of request
+        """
+        headers = self.headers
+        headers["Authorization"] = f"Bearer {token}"
+        path = self.url + "/logout"
+        response = requests.get(url=path, headers=headers)
+
+        return response
 
     def registration(self, body: dict) -> Response:
         """Endpoint for registration of user
