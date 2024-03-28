fbb3d08408f957a2e9416cbede4220240cb91816
TetianaPerinha <68566253+TetianaPerinha@users.noreply.github.com>
2024-03-28 11:03:18 -0500

    IL-308 Change password/reset. Add configs.py

---
+++ configs.py
@@ -0,0 +1,45 @@
+# HOST = 'https://dev.api.it-sl.ru'
+
+
+HOST = "http://localhost:8083"
+# HOST = 'https://iced-latte.uk/backend'
+HOST_DB = "192.168.1.66"
+# HOST_DB = '172.20.10.3'
+# HOST_DB = 'localhost'
+
+PORT_DB = "5432"
+DB_NAME = "testdb"
+DB_USER = "postgres"
+DB_PASS = "postgres"
+
+# data for creating user#1
+email = "icedlate.test@gmail.com"
+password = "password12345"
+firstName = "TestUser"
+lastName = "TestUser"
+EMAIL_DOMAIN = "gmail.com"
+EMAIL_LOCAL_PART = "icedlate.test+"
+
+# DATA to connect to the qmail on imap.gmail.com server for user#1
+gmail_password = "qved ehhy ttuk lvne"
+imap_server = "imap.gmail.com"
+email_address_to_connect = "icedlate.test@gmail.com"
+
+# data for creating user#2
+email2 = "icedlate.test@gmail.com"
+password2 = "password12345"
+firstName2 = "TestUser"
+lastName2 = "TestUser"
+EMAIL_DOMAIN2 = "gmail.com"
+EMAIL_LOCAL_PART2 = "icedlate2.test+"
+
+# DATA to connect to the qmail on imap.gmail.com server for user#2
+gmail_password2 = "mcio okxb yyxz lbmo"
+imap_server2 = "imap.gmail.com"
+email_address_to_connect2 = "icedlate2.test@gmail.com"
+
+# iced-late mail
+email_iced_late = "youricedlatteshop@gmail.com"
+
+DEFAULT_PASSWORD = "pass1234"
+JWT_SECRET = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970"
